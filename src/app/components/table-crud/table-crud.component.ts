861e234bef0a485ea8255950caa6c28bbacf9864
oscar montes <omontes@enigmadev.com>
2023-01-19 08:50:13 -0500

    Implementacion de cruds
    Implementacion de cruds.

--- src/app/components/table-crud/table-crud.component.ts
+++ src/app/components/table-crud/table-crud.component.ts
@@ -6,165 +6,223 @@ import { environment } from 'src/environments/environment';
 import School, { SchoolsDto } from 'src/app/interfaces/school.interface';
 
 @Component({
-  selector: 'app-table-crud',
-  templateUrl: './table-crud.component.html',
-  styleUrls: ['./table-crud.component.scss'],
+    selector: 'app-table-crud',
+    templateUrl: './table-crud.component.html',
+    styleUrls: ['./table-crud.component.scss'],
 })
 export class TableCrudComponent implements OnInit {
-  Delete = 'Delete';
+    Delete = 'Delete';
 
-  productDialog: boolean = false;
+    productDialog: boolean = false;
 
-  schools: School[] = [];
+    schools: School[] = [];
 
-  school: School = {
-    message: '',
-    schoolsDto:  [ {
-        id: 0,
-        name: '',
-      }
-    ]
-  };
-
-  schoolsDto: SchoolsDto = {
-    id: 0,
-    name: '',
-  }
-
-  submitted: boolean = true;
-
-  url = '/api/school';
-
-  headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${environment.token}`,
-  });
-
-  constructor(
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
-    private http: HttpClient
-  ) {}
-
-  ngOnInit() {
-    console.log('NgOnInit');
-
-    this.http
-      .get('/api/school/', { headers: this.headers })
-      .subscribe((value: any) => {
-        this.schools = value.schoolsDto;
-      });
-  }
-
-  openNew() {
-    this.school = {
-      message: '',
-      schoolsDto:[{
-          id: 0,
-          name: '',
-        }
-    ]
+    school: School = {
+        message: '',
+        schoolsDto: [
+            {
+                id: 0,
+                name: '',
+            },
+        ],
     };
 
-    this.schoolsDto =    {
+    schoolsDto: SchoolsDto = {
         id: 0,
         name: '',
-      };
+    };
 
-    this.submitted = false;
-    this.productDialog = true;
-  }
+    submitted: boolean = true;
 
-  editProduct(schoolEdit: School) {
-    this.productDialog = true;
-    this.school = schoolEdit;
-    this.schoolsDto = schoolEdit.schoolsDto[0];
-    console.log('schoolEdit',  this.school);
-    console.log('schoolsDto',  this.school);
-  }
+    url = '/api/school';
 
-  deleteProduct(schoolDelete: School) {
-    console.log('schoolDelete', schoolDelete);
-    this.confirmationService.confirm({
-      message:
-        'Are you sure you want to delete ' + schoolDelete.schoolsDto[0].name + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.http
-          .delete(this.url + `/${schoolDelete.schoolsDto[0].id}`, {
-            headers: this.headers,
-          })
-          .subscribe((value) => {
-            this.updateView();
-          });
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Successful',
-          detail: 'Product Deleted',
-          life: 3000,
-        });
-      },
+    headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${environment.token}`,
     });
-  }
 
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
+    constructor(
+        private messageService: MessageService,
+        private confirmationService: ConfirmationService,
+        private http: HttpClient
+    ) {}
 
-  saveSchool() {
-    this.submitted = true;
-    if (this.school.schoolsDto[0].name.trim()) {
-      if (this.school.schoolsDto[0].id !== 0) {
-        this.http
-          .put(this.url, this.school.schoolsDto, { headers: this.headers })
-          .subscribe((value) => {
-            this.updateView();
-          });
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Successful',
-          detail: 'Product Updated',
-          life: 3000,
+    ngOnInit() {
+        console.log('NgOnInit');
+        this.updateView();
+    }
+
+    openNew() {
+        this.school = {
+            message: '',
+            schoolsDto: [
+                {
+                    id: 0,
+                    name: '',
+                },
+            ],
+        };
+
+        this.schoolsDto = {
+            id: 0,
+            name: '',
+        };
+
+        this.submitted = false;
+        this.productDialog = true;
+    }
+
+    editProduct(schoolEdit: SchoolsDto) {
+        console.log(schoolEdit);
+        console.log(this.school.schoolsDto[0]);
+        this.school.schoolsDto[0] = schoolEdit;
+        this.productDialog = true;
+    }
+
+    deleteProduct(schoolDelete: SchoolsDto) {
+        console.log('schoolDelete', schoolDelete);
+        this.confirmationService.confirm({
+            message: `Are you sure you want to delete ${schoolDelete.name} ?`,
+            header: 'Confirm',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                console.log(this.url + `/${schoolDelete.id}`);
+                this.http
+                    .delete(this.url + '/' + schoolDelete.id, {
+                        headers: this.headers,
+                    })
+                    .subscribe(
+                        (value) => {
+                            this.messageService.add({
+                                severity: 'success',
+                                summary: 'Successful',
+                                detail: 'Product Deleted',
+                                life: 3000,
+                            });
+                            this.updateView();
+                        },
+                        (error) => {
+                            this.messageService.add({
+                                severity: 'error',
+                                summary: 'Error',
+                                detail: 'We could not delete school',
+                                life: 3000,
+                            });
+                            console.log(`Sucedio este error: ${error}`);
+                        }
+                    );
+            },
         });
-      } else {
-        //Consumir endpoint guardado
+    }
+
+    hideDialog() {
+        this.productDialog = false;
+        this.submitted = false;
+    }
+
+    saveSchool() {
+        this.submitted = true;
+        if (this.school.schoolsDto[0].name.trim()) {
+            if (this.school.schoolsDto[0].id !== 0) {
+                this.http
+                    .put(this.url, this.school.schoolsDto[0], {
+                        headers: this.headers,
+                    })
+                    .subscribe(
+                        (value) => {
+                            this.messageService.add({
+                                severity: 'success',
+                                summary: 'Successful',
+                                detail: 'School Updated',
+                                life: 3000,
+                            });
+                            this.updateView();
+                        },
+                        (error) => {
+                            this.messageService.add({
+                                severity: 'error',
+                                summary: 'Error',
+                                detail: 'We could not update school',
+                                life: 3000,
+                            });
+                            console.log(`Ocurrio este error: ${error}`);
+                        }
+                    );
+            } else {
+                //Consumir endpoint guardado
+                console.log(this.school.schoolsDto);
+                this.http
+                    .post(this.url, this.school.schoolsDto[0], {
+                        headers: this.headers,
+                    })
+                    .subscribe(
+                        (value) => {
+                            this.messageService.add({
+                                severity: 'success',
+                                summary: 'Successful',
+                                detail: 'School Created',
+                                life: 3000,
+                            });
+                            this.updateView();
+                        },
+                        (error) => {
+                            this.messageService.add({
+                                severity: 'error',
+                                summary: 'Error',
+                                detail: 'We could not create school',
+                                life: 3000,
+                            });
+                            console.log(
+                                `Ocurrio este error: ${JSON.stringify(error)}`
+                            );
+                        }
+                    );
+            }
+            this.productDialog = false;
+        }
         console.log(this.school);
+    }
+
+    updateView = () => {
         this.http
-          .post(this.url, this.school.schoolsDto, { headers: this.headers })
-          .subscribe((value) => {
+            .get(this.url, { headers: this.headers })
+            .subscribe((value: any) => {
+                this.schools = value.schoolsDto;
+            });
+
+        // this.http
+        //     .get('/api/school/', { headers: this.headers })
+        //     .subscribe((value: any) => {
+        //         this.schools = value.schoolsDto;
+        //     });
+    };
+
+    findIndexById(id: number): number {
+        let index = -1;
+        for (let i = 0; i < this.schools.length; i++) {
+            if (this.schools[i].schoolsDto[0].id === id) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    searchFilter = (event: any) => {
+        const keyWord = event.target.value;
+        if (keyWord === '') {
             this.updateView();
-          });
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Successful',
-          detail: 'Product Created',
-          life: 3000,
-        });
-      }
-      this.productDialog = false;
-    }
-    console.log(this.school);
-  }
-
-  updateView = () => {
-    this.http
-      .get(this.url, { headers: this.headers })
-      .subscribe((value: any) => {
-        this.schools = value.schoolsDto;
-      });
-  };
-
-  findIndexById(id: number): number {
-    let index = -1;
-    for (let i = 0; i < this.schools.length; i++) {
-      if (this.schools[i].schoolsDto[0].id === id) {
-        index = i;
-        break;
-      }
-    }
-
-    return index;
-  }
+            console.log('Traer todo el resultado.');
+        } else {
+            const arrayToFilter = [...this.schools];
+            this.schools = arrayToFilter.filter((school: any) => {
+                if (school.name.includes(keyWord)) {
+                    return school;
+                }
+            });
+            console.log(`Copia de schools: ${JSON.stringify(arrayToFilter)}`);
+            console.log('filtrando', event.target.value);
+        }
+    };
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -5,248 +5,273 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import User from 'src/app/interfaces/users.interface';
 interface Roles {
-    name: string,
-    code: number
+    name: string;
+    code: number;
 }
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css'],
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-  Delete = 'Delete';
-  errorMessage?: string;
+    Delete = 'Delete';
+    errorMessage?: string;
 
-  roles: Roles[]  = [];
-  selectedRoles1!: Roles;
+    roles: Roles[] = [];
+    selectedRoles1!: Roles;
 
-  productDialog: boolean = false;
+    productDialog: boolean = false;
 
-  users: User[] = [];
-  usersCambioRol: User[] = [];
+    users: User[] = [];
+    usersCambioRol: User[] = [];
 
-  user: User = {
-    message: '',
-    usersDto: [],
-  };
-
-  submitted: boolean = true;
-  regexPassword = new RegExp("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.]+@[a-zA-Z0-9.]+$");
-  regexEmail= new RegExp("^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}$");
-
-  url = '/api/users';
-
-  headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${environment.token}`,
-  });
-
-  constructor(
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
-    private http: HttpClient
-  ) {
-
-    this.roles = [
-        {name: 'Admin', code: 1},
-        {name: 'Employee', code: 2}
-    ];
-
-  }
-
-  ngOnInit() {
-    this.http.get<any>(this.url, { headers: this.headers }).subscribe({
-      next: value => {
-        this.users = value.usersDto;
-        this.users.forEach((value: any) => {
-          if (value.rol === '1') {
-            value.rol = 'Admin';
-          } else {
-            value.rol = 'Employee';
-          }
-          console.log(value.rol);
-        });
-      },
-      error: error => {
-        console.log(error);
-        this.errorMessage = error.message;
-          if(error.status == 403){
-            this.messageService.add({
-              severity: 'error',
-              summary: 'error',
-              detail: this.errorMessage,
-              life: 3000,
-            });
-          }else{
-            this.messageService.add({
-              summary: 'error',
-              detail: error.error.message,
-              life: 3000,
-            });
-          }
-
-      }
-  });
-  }
-
-  openNew() {
-    this.user = {
-      message: '',
-      usersDto: [{ id: 0, email: '', passwd: '', name: '', rol: 0 }],
+    user: User = {
+        message: '',
+        usersDto: [],
     };
-    this.submitted = false;
-    this.productDialog = true;
-  }
 
-  editProduct(user: User) {
-    this.productDialog = true;
-    this.user = user;
-    if(this.user.usersDto[0].rol == 1 ){
-        this.selectedRoles1 = {name: 'Admin', code: 1};
-    }else{
-        this.selectedRoles1 = {name: 'Employee', code: 2};
+    submitted: boolean = true;
+    regexPassword = new RegExp(
+        "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.]+@[a-zA-Z0-9.]+$"
+    );
+    regexEmail = new RegExp('^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}$');
+
+    url = '/api/users';
+
+    headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${environment.token}`,
+    });
+
+    constructor(
+        private messageService: MessageService,
+        private confirmationService: ConfirmationService,
+        private http: HttpClient
+    ) {
+        this.roles = [
+            { name: 'Admin', code: 1 },
+            { name: 'Employee', code: 2 },
+        ];
     }
-  }
 
-  deleteProduct(school: User) {
-    console.log('user', school);
-    this.confirmationService.confirm({
-      message:
-        'Are you sure you want to delete ' + school.usersDto[0].name + '?',
-      header: 'Confirm',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-
-    this.http.delete(this.url + `/${school.usersDto[0].id}`, {headers: this.headers}).subscribe({
-      next: value => {
-        this.updateView();
-        this.messageService.add({
-            severity: 'success',
-            summary: 'Successful',
-            detail: 'User Deleted',
-            life: 3000,
-          });
-      },
-      error: error => {
-        console.log(error);
-        this.errorMessage = error.message;
-          if(error.status == 403){
-            this.messageService.add({
-              severity: 'error',
-              summary: 'error',
-              detail: this.errorMessage,
-              life: 3000,
-            });
-          }else{
-            this.messageService.add({
-              severity: 'error',
-              summary: 'error',
-              detail: error.error.message,
-              life: 3000,
-            });
-          }
-
-      }
-  });
- },
-});
-}
-
-  hideDialog() {
-    this.productDialog = false;
-    this.submitted = false;
-  }
-
-  saveSchool() {
-    this.submitted = true;
-    if (this.user.usersDto[0].name.trim()) {
-        this.user.usersDto[0].rol = this.selectedRoles1.code;
-      if (this.user.usersDto[0].id !== 0) {
-        this.http.put(this.url, this.user.usersDto[0], { headers: this.headers }).subscribe({
-            next: value => {
-              this.updateView();
-              this.messageService.add({
-                  severity: 'success',
-                  summary: 'Successful',
-                  detail: 'User Updated',
-                  life: 3000,
-                });
-            },
-            error: error => {
-              console.log(error);
-              this.errorMessage = error.message;
-                if(error.status == 403){
-                  this.messageService.add({
-                    severity: 'error',
-                    summary: 'error',
-                    detail: this.errorMessage,
-                    life: 3000,
-                  });
-                }else{
-                  this.messageService.add({
-                    severity: 'error',
-                    summary: 'error',
-                    detail: error.error.message,
-                    life: 3000,
-                  });
-                }
-
-            }
-        });
-      } else {
-        this.http.post(this.url, this.user.usersDto[0], { headers: this.headers }).subscribe({
+    ngOnInit() {
+        this.http.get<any>(this.url, { headers: this.headers }).subscribe({
             next: (value) => {
-              this.updateView();
-              this.messageService.add({
-                  severity: 'success',
-                  summary: 'Successful',
-                  detail: 'User Created',
-                  life: 3000,
+                this.users = value.usersDto;
+                this.users.forEach((value: any) => {
+                    if (value.rol === '1') {
+                        value.rol = 'Admin';
+                    } else {
+                        value.rol = 'Employee';
+                    }
+                    console.log(value.rol);
                 });
             },
             error: (error) => {
-                if(error.status == 403){
-                 this.errorMessage = error.message;
-                  this.messageService.add({
-                    severity: 'error',
-                    summary: 'error',
-                    detail: this.errorMessage,
-                    life: 3000,
-                  });
-                }else{
-                  this.messageService.add({
-                    severity: 'error',
-                    summary: 'error',
-                    detail: error.error.message,
-                    life: 3000,
-                  });
+                console.log(error);
+                this.errorMessage = error.message;
+                if (error.status == 403) {
+                    this.messageService.add({
+                        severity: 'error',
+                        summary: 'error',
+                        detail: this.errorMessage,
+                        life: 3000,
+                    });
+                } else {
+                    this.messageService.add({
+                        summary: 'error',
+                        detail: error.error.message,
+                        life: 3000,
+                    });
                 }
-
-            }
+            },
         });
-      }
-      this.productDialog = false;
     }
-    console.log(this.user);
-  }
 
-  updateView = () => {
-    this.http
-      .get(this.url, { headers: this.headers })
-      .subscribe((value: any) => {
-        this.users = value.usersDto;
-      });
-  };
-
-  findIndexById(id: number): number {
-    let index = -1;
-    for (let i = 0; i < this.users.length; i++) {
-      if (this.users[i].usersDto[i].id === id) {
-        index = i;
-        break;
-      }
+    openNew() {
+        this.user = {
+            message: '',
+            usersDto: [{ id: 0, email: '', passwd: '', name: '', rol: 0 }],
+        };
+        this.submitted = false;
+        this.productDialog = true;
     }
-    return index;
-  }
 
+    editProduct(user: User) {
+        this.productDialog = true;
+        this.user = user;
+        if (this.user.usersDto[0].rol == 1) {
+            this.selectedRoles1 = { name: 'Admin', code: 1 };
+        } else {
+            this.selectedRoles1 = { name: 'Employee', code: 2 };
+        }
+    }
+
+    deleteProduct(school: User) {
+        console.log('user', school);
+        this.confirmationService.confirm({
+            message:
+                'Are you sure you want to delete ' +
+                school.usersDto[0].name +
+                '?',
+            header: 'Confirm',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.http
+                    .delete(this.url + `/${school.usersDto[0].id}`, {
+                        headers: this.headers,
+                    })
+                    .subscribe({
+                        next: (value) => {
+                            this.updateView();
+                            this.messageService.add({
+                                severity: 'success',
+                                summary: 'Successful',
+                                detail: 'User Deleted',
+                                life: 3000,
+                            });
+                        },
+                        error: (error) => {
+                            console.log(error);
+                            this.errorMessage = error.message;
+                            if (error.status == 403) {
+                                this.messageService.add({
+                                    severity: 'error',
+                                    summary: 'error',
+                                    detail: this.errorMessage,
+                                    life: 3000,
+                                });
+                            } else {
+                                this.messageService.add({
+                                    severity: 'error',
+                                    summary: 'error',
+                                    detail: error.error.message,
+                                    life: 3000,
+                                });
+                            }
+                        },
+                    });
+            },
+        });
+    }
+
+    hideDialog() {
+        this.productDialog = false;
+        this.submitted = false;
+    }
+
+    saveSchool() {
+        this.submitted = true;
+        if (this.user.usersDto[0].name.trim()) {
+            this.user.usersDto[0].rol = this.selectedRoles1.code;
+            if (this.user.usersDto[0].id !== 0) {
+                this.http
+                    .put(this.url, this.user.usersDto[0], {
+                        headers: this.headers,
+                    })
+                    .subscribe({
+                        next: (value) => {
+                            this.updateView();
+                            this.messageService.add({
+                                severity: 'success',
+                                summary: 'Successful',
+                                detail: 'User Updated',
+                                life: 3000,
+                            });
+                        },
+                        error: (error) => {
+                            console.log(error);
+                            this.errorMessage = error.message;
+                            if (error.status == 403) {
+                                this.messageService.add({
+                                    severity: 'error',
+                                    summary: 'error',
+                                    detail: this.errorMessage,
+                                    life: 3000,
+                                });
+                            } else {
+                                this.messageService.add({
+                                    severity: 'error',
+                                    summary: 'error',
+                                    detail: error.error.message,
+                                    life: 3000,
+                                });
+                            }
+                        },
+                    });
+            } else {
+                this.http
+                    .post(this.url, this.user.usersDto[0], {
+                        headers: this.headers,
+                    })
+                    .subscribe({
+                        next: (value) => {
+                            this.updateView();
+                            this.messageService.add({
+                                severity: 'success',
+                                summary: 'Successful',
+                                detail: 'User Created',
+                                life: 3000,
+                            });
+                        },
+                        error: (error) => {
+                            if (error.status == 403) {
+                                this.errorMessage = error.message;
+                                this.messageService.add({
+                                    severity: 'error',
+                                    summary: 'error',
+                                    detail: this.errorMessage,
+                                    life: 3000,
+                                });
+                            } else {
+                                this.messageService.add({
+                                    severity: 'error',
+                                    summary: 'error',
+                                    detail: error.error.message,
+                                    life: 3000,
+                                });
+                            }
+                        },
+                    });
+            }
+            this.productDialog = false;
+        }
+        console.log(this.user);
+    }
+
+    updateView = () => {
+        this.http
+            .get(this.url, { headers: this.headers })
+            .subscribe((value: any) => {
+                this.users = value.usersDto;
+            });
+    };
+
+    findIndexById(id: number): number {
+        let index = -1;
+        for (let i = 0; i < this.users.length; i++) {
+            if (this.users[i].usersDto[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    validarEmail = () => {
+        const validate = true;
+        console.log('Validando email...');
+        return validate;
+    };
+
+    validarPassword = () => {
+        let validate = true;
+        if (this.user.usersDto[0].passwd === '') {
+            validate = true;
+        } else {
+            validate = false;
+            console.log('Validando password...', this.user.usersDto[0].passwd);
+        }
+        return validate;
+    };
 }
